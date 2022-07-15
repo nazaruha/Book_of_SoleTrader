1e5429df7240dc0fddb9f7ab8983c3c683fda405
Nazar <fedunnazar20122001@gmail.com>
2022-07-15 11:08:18 +0300

    Add DB creating and connection to it

--- Worker_DB/Program.cs
+++ Worker_DB/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +11,34 @@ namespace Worker_DB
 {
     public class Program
     {
-        static string DbName = "РРО";
+        static string DbName = "РРО2";
         static string dirSql = "SqlTables";
         static SqlConnection con;
         static SqlCommand cmd;
 
         static void Main(string[] args)
         {
-            
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfiguration configuration = builder.Build();
+            string connectionDB = configuration.GetConnectionString("DefaultConnection");
+            con = new SqlConnection(connectionDB);
+            con.Open();
+
+            if (!IsDBExists())
+            {
+                cmd.CommandText = $"CREATE DATABASE {DbName}";
+                cmd.ExecuteNonQuery();
+                Console.WriteLine(con.ToString());
+            }
+            con = new SqlConnection(connectionDB + $"Initial Catalog={DbName}");
+            con.Open();
+            cmd = con.CreateCommand();
         }
 
         static bool IsDBExists()
