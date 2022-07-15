fb6aef803b2cb6dca763dc44930a21f3e45330ce
Nazar <fedunnazar20122001@gmail.com>
2022-07-15 11:25:11 +0300

    create sql command to create tblCustomers

---
+++ Worker_DB/SqlTables/tblManufacturers - Copy.sql
@@ -0,0 +1,8 @@
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[tblCustomers]'))
+EXEC dbo.sp_executesql @statement = N'
+CREATE TABLE [dbo].[tblCustomers](
+  [Id] [int] IDENTITY PRIMARY KEY NOT NULL,
+  [Name] [nvarchar](100) NOT NULL,
+  [Phone] [nvarchar](50) NOT NULL,
+  [PurchaseSum] [int] NOT NULL,
+);'
