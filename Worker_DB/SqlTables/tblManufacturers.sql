9a178ec1992d60e73bbd2dbdbb303f49f2e78780
Nazar <fedunnazar20122001@gmail.com>
2022-07-15 11:31:48 +0300

    Modify column 'Name' to unique

--- Worker_DB/SqlTables/tblManufacturers.sql
+++ Worker_DB/SqlTables/tblManufacturers.sql
@@ -2,5 +2,5 @@ IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[tbl
 EXEC dbo.sp_executesql @statement = N'
 CREATE TABLE [dbo].[tblManufacturers](
   [Id] [int] IDENTITY PRIMARY KEY NOT NULL,
-  [Name] [nvarchar](100) NOT NULL,
+  [Name] [nvarchar](100) NOT NULL UNIQUE,
 );'
