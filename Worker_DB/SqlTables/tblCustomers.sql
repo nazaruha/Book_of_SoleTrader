56184a179f0bb078f17ae107be9202d0693287ae
Nazar <fedunnazar20122001@gmail.com>
2022-07-15 11:33:02 +0300

    Modify column 'Phone' to unique

--- Worker_DB/SqlTables/tblCustomers.sql
+++ Worker_DB/SqlTables/tblCustomers.sql
@@ -3,6 +3,6 @@ EXEC dbo.sp_executesql @statement = N'
 CREATE TABLE [dbo].[tblCustomers](
   [Id] [int] IDENTITY PRIMARY KEY NOT NULL,
   [Name] [nvarchar](100) NOT NULL,
-  [Phone] [nvarchar](50) NOT NULL,
+  [Phone] [nvarchar](50) NOT NULL UNIQUE,
   [PurchaseSum] [int] NOT NULL,
 );'
