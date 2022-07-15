66953e7abe4db23dcc6ebff25cec10fc1b8b8589
Nazar <fedunnazar20122001@gmail.com>
2022-07-15 10:53:42 +0300

    create fields to work with local db

--- Worker_DB/Program.cs
+++ Worker_DB/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Worker_DB
 {
-    internal class Program
+    public class Program
     {
+        static string DbName = "РРО";
+        static string dirSql = "SqlTables";
+        static SqlConnection con;
+        static SqlCommand cmd;
+
         static void Main(string[] args)
         {
+            
         }
+
+        
     }
 }
