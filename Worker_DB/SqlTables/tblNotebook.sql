add5f30402ca59d3b8fc697e49c5244c1cfe37c0
Nazar <fedunnazar20122001@gmail.com>
2022-07-15 11:52:25 +0300

    Modify tblNotebook

--- Worker_DB/SqlTables/tblNotebook.sql
+++ Worker_DB/SqlTables/tblNotebook.sql
@@ -1,17 +1,22 @@
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[tblStorage]'))
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[tblNotebook]'))
 EXEC dbo.sp_executesql @statement = N'
-CREATE TABLE [dbo].[tblStorage](
-  [ManufacturerId] [int] NOT NULL,
+CREATE TABLE [dbo].[tblNotebook](
+  [Id] [int] PRIMARY KEY NOT NULL,
+  [Date] [date] NOT NULL,
   [ProductId] [int] NOT NULL,
-  [Price] [int] NOT NULL,
+  [ManufacturerId] [int] NOT NULL,
   [Count] [int] NOT NULL,
+  [TotalSum] [int] NOT NULL,
+  [CuromerId] [int] NOT NULL,
+  [Discount] [int] NULL,
 
-  CONSTRAINT [PK_tblStorage] PRIMARY KEY CLUSTERED
-  ( [ManufacturerId] ASC, [ProductId] ASC ),
+  CONSTRAINT [FK_tblNotebook_tblCustomers] FOREIGN KEY([CuromerId])
+   REFERENCES [dbo].[tblCustomers] ([Id]),
 
-  CONSTRAINT [FK_tblStorage_tblManufacturers] FOREIGN KEY([ManufacturerId])
-   REFERENCES [dbo].[tblManufacturers] ([Id]),
+  CONSTRAINT [FK_tblNotebook_tblManufacturers] FOREIGN KEY([ManufacturerId])
+   REFERENCES [dbo].[tblManufacturers] ([Id])
 
-  CONSTRAINT [FK_tblStorage_tblGroceries] FOREIGN KEY([ProductId])
+  CONSTRAINT [FK_tblNotebook_tblGroceries] FOREIGN KEY([ProductId])
    REFERENCES [dbo].[tblGroceries] ([Id]),
+
 );'
