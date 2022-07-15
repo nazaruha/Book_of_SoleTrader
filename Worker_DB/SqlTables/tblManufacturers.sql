d2f51e200020edecfafc1ef3cd2c9b0a3f58242e
Nazar <fedunnazar20122001@gmail.com>
2022-07-15 11:21:47 +0300

    create sql command to create tblManufacturers

---
+++ Worker_DB/SqlTables/tblManufacturers.sql
@@ -0,0 +1,6 @@
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[tblManufacturers]'))
+EXEC dbo.sp_executesql @statement = N'
+CREATE TABLE [dbo].[tblManufacturers](
+  [Id] [int] IDENTITY PRIMARY KEY NOT NULL,
+  [Name] [nvarchar](100) NOT NULL,
+);'
