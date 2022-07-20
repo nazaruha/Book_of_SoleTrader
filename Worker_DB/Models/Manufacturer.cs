218020c8dccf0855283661acf3018a5f62162319
Nazar <fedunnazar20122001@gmail.com>
2022-07-20 14:11:09 +0300

    Add class for manufacturers

---
+++ Worker_DB/Models/Manufacturer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker_DB.Models
+{
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+
+        public Manufacturer()
+        {
+
+        }
+    }
+}
