d123a74f5524d6a51f646fb7066e7665115774e7
Nazar <fedunnazar20122001@gmail.com>
2022-07-20 15:15:22 +0300

    add class for Product in tblGroceries

---
+++ Worker_DB/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker_DB.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public Product()
+        {
+
+        }
+    }
+}
