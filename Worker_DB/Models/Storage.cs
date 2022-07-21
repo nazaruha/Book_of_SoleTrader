4478388b0c11bd94a93b5e9f100aac9ac37a9e15
Nazar <fedunnazar20122001@gmail.com>
2022-07-21 11:18:11 +0300

    create new class for storage objects

---
+++ Worker_DB/Models/Storage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker_DB.Models
+{
+    public class Storage
+    {
+        public int ManufacturerId { get; set; }
+        public int ProductId { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+    }
+}
