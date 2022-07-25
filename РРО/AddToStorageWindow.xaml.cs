1e8ad5b553276d070b3066773e53f187ca7312db
Nazar <fedunnazar20122001@gmail.com>
2022-07-25 15:57:49 +0300

    adding products to storage is successfully

--- РРО/AddToStorageWindow.xaml.cs
+++ РРО/AddToStorageWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,6 +22,7 @@ namespace РРО
     public partial class AddToStorageWindow : Window
     {
         private SqlCommand cmd { get; set; }
+        private bool flag = false;
 
         public AddToStorageWindow(SqlCommand cmd)
         {
@@ -43,6 +45,24 @@ namespace РРО
             }
         }
 
+        private int GetManufacturerId()
+        {
+            if (cbManufacturers.SelectedIndex == -1) return -1;
+            cmd.CommandText = "SELECT Id " +
+                "FROM tblManufacturers " +
+                @"WHERE Name = @NameField";
+            cmd.Parameters.AddWithValue(@"NameField", cbManufacturers.SelectedItem.ToString());
+
+            using (var reader = cmd.ExecuteReader())
+            {
+                reader.Read();
+                int id = int.Parse(reader["Id"].ToString());
+                cmd.Parameters.Clear();
+                return id;
+            }
+           
+        }
+
         private void GetGroceries()
         {
             cmd.CommandText = "SELECT Name " +
@@ -56,9 +76,101 @@ namespace РРО
             }
         }
 
+        private int GetProductId()
+        {
+            if (cbGroceries.SelectedIndex == -1) return -1;
+            cmd.CommandText = "SELECT Id " +
+                "FROM tblGroceries " +
+                @"WHERE Name = @NameField";
+            cmd.Parameters.AddWithValue(@"@NameField", cbGroceries.SelectedItem.ToString());
+
+            using (var reader = cmd.ExecuteReader())
+            {
+                reader.Read();
+                int id = int.Parse(reader["Id"].ToString());
+                cmd.Parameters.Clear();
+                return id;
+            }
+
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void cbGroceries_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbManufacturers.SelectedIndex == -1) return;
+            GetStorageInfo();
+            
+        }
+
+        private void cbManufacturers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbGroceries.SelectedIndex == -1) return;
+            GetStorageInfo();
+        }
+
+        private void GetStorageInfo()
+        {
+            int manufacturerId = GetManufacturerId();
+            int productId = GetProductId();
+            cmd.CommandText = "SELECT Price, Count " +
+                "FROM tblStorage " +
+                @"WHERE ManufacturerId = @ManufacturerIdField AND ProductId = @ProductIdField";
+            cmd.Parameters.AddWithValue(@"@ManufacturerIdField", manufacturerId);
+            cmd.Parameters.AddWithValue(@"@ProductIdField", productId);
+
+            using (var reader = cmd.ExecuteReader())
+            {
+                reader.Read();
+                try
+                {
+                    if (reader["Price"] != null && reader["Count"] != null)
+                    {
+                        txtPrice.Text = reader["Price"].ToString();
+                        txtCount.Text = reader["Count"].ToString();
+                        flag = true;
+                    }
+                }
+                catch
+                {
+                    txtPrice.Clear();
+                    txtCount.Clear();
+                    flag = false;
+                }
+                cmd.Parameters.Clear();
+            }
+        }
+
         private void btnAddProduct_Click(object sender, RoutedEventArgs e)
         {
-
+            int manufacturerId = GetManufacturerId();
+            int productId = GetProductId();
+            if (flag)
+            {
+                cmd.CommandText = "UPDATE tblStorage " +
+                    @"SET Price = @PriceField, [Count] = @CountField " +
+                    @"WHERE ManufacturerId = @ManufacturerIdField AND ProductId = @ProductIdField";
+            }
+            else
+            {
+                cmd.CommandText = "INSERT INTO tblStorage " +
+                    "( " +
+                    "ManufacturerId, ProductId, Price, [Count] " +
+                    ") " +
+                    "VALUES ( " +
+                    @"@ManufacturerIdField, @ProductIdField, @PriceField, @CountField " +
+                    ")";
+            }
+            cmd.Parameters.AddWithValue(@"PriceField", int.Parse(txtPrice.Text));
+            cmd.Parameters.AddWithValue(@"@CountField", int.Parse(txtCount.Text));
+            cmd.Parameters.AddWithValue(@"@ManufacturerIdField", manufacturerId);
+            cmd.Parameters.AddWithValue(@"@ProductIdField", productId);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Data has been updated");
         }
     }
 }
