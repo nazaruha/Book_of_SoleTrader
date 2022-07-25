6d1f0580b979f7525c4c4d922730bfc2c91067ef
Nazar <fedunnazar20122001@gmail.com>
2022-07-25 14:05:16 +0300

    create window for adding product to storage

---
+++ РРО/AddToStorageWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace РРО
+{
+    /// <summary>
+    /// Interaction logic for AddToStorageWindow.xaml
+    /// </summary>
+    public partial class AddToStorageWindow : Window
+    {
+        public AddToStorageWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
