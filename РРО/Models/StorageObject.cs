5d5eb54b8119cf368aab39700570087026fd8125
Nazar <fedunnazar20122001@gmail.com>
2022-07-30 23:58:37 +0300

    create class of sotrage's objects

---
+++ РРО/Models/StorageObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace РРО.Models
+{
+    public class StorageObject
+    {
+        public string Manufacturer { get; set; }
+        public string Product { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+    }
+}
