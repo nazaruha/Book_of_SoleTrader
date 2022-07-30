aa16f9e1eea2d59eaf475ec2c44030e6d771530d
Nazar <fedunnazar20122001@gmail.com>
2022-07-30 15:38:11 +0300

    delete unnecessary rows

--- РРО/Scripts/viewtblNotebook_OFFSET.sql
+++ РРО/Scripts/viewtblNotebook_OFFSET.sql
@@ -1,12 +1,3 @@
---select n.Id, n.[Date], m.[Name] as Manufacturer, g.[Name] as Product, n.[Count], n.TotalSum, c.[Name] as Customer, c.Phone, n.Discount
---from tblNotebook as n
---inner join tblGroceries as g
---on n.ProductId = g.Id
---inner join tblManufacturers as m
---on n.ManufacturerId = m.Id
---inner join tblCustomers as c
---on n.CustomerId = c.Id
-
 SELECT n.Id, n.[Date], m.[Name] as Manufacturer, g.[Name] as Product, n.[Count], n.TotalSum, c.[Name] as Customer, c.Phone, n.Discount
 FROM tblNotebook as n
 INNER JOIN tblGroceries as g
