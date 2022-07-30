66322a466805508b7be2f657beffe793bf2bca7b
Nazar <fedunnazar20122001@gmail.com>
2022-07-30 23:57:07 +0300

    edit script

--- РРО/Scripts/viewtblStorage.sql
+++ РРО/Scripts/viewtblStorage.sql
@@ -1,1 +1,6 @@
-SELECT * FROM tblStorage
+select m.[Name] as Manufacturer, g.[Name] as Product, Price, [Count]
+from tblStorage as s
+inner join tblManufacturers as m
+on s.ManufacturerId = m.Id
+inner join tblGroceries as g
+on s.ProductId = g.Id
