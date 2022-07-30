74bd47e02af809235ec804e59872d77929891b44
Nazar <fedunnazar20122001@gmail.com>
2022-07-30 16:53:24 +0300

    add manufacturer class with props

---
+++ РРО/Models/Manufacturer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace РРО.Models
+{
+    public class Manufacturer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
